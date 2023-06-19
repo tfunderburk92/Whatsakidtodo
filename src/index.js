81a1d0ed9d9f91e6b37a241bc5f239d269ce2ff4
Tina Funderburk <tina.funderburk@yahoo.com>
2023-06-19 07:14:42 -0400

    initial changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const prepare = () => {
+  if (process.env.NODE_ENV === 'development') {  // prevents the mock server from working in a deployed, production env
+    const { worker } = require('./mocks/browser.js')
+    return worker.start() // Comment out this return statement to disable the mock api
+  }
+  return Promise.resolve()
+}
+
+prepare().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+
+    </React.StrictMode>
+  );
+})
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
